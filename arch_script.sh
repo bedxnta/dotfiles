1a1039b5356d6f3b96ef04cb8035c1fb85ddcc32
Bedanta D <bedanta@duck.com>
2024-08-25 23:08:15 +0530

    Update arch_script.sh

--- arch_script.sh
+++ arch_script.sh
@@ -21,7 +21,7 @@ sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji
 
 # Install packages using yay and pacman
 yay -S --noconfirm brave-bin visual-studio-code-bin prismlauncher-qt5-bin ani-cli
-sudo pacman -S --noconfirm libreoffice discord mpv spotify p7zip qbittorrent eog evince jre22-openjdk fastfetch
+sudo pacman -S --noconfirm libreoffice-still discord mpv spotify-launcher p7zip qbittorrent eog evince jre22-openjdk fastfetch
 
 # Final message
 echo "Setup complete!"
